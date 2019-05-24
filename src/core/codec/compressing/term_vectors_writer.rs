3e87a8c3f36e5ee795361b59c94ee0d4db89904e
Xiaoguang Sun <sunxiaoguang@zhihu.com>
2019-05-24 16:25:20 +0800

    Add license
    Signed-off-by: Xiaoguang Sun <sunxiaoguang@zhihu.com>

--- src/core/codec/compressing/term_vectors_writer.rs
+++ src/core/codec/compressing/term_vectors_writer.rs
@@ -1,3 +1,16 @@
+// Copyright 2019 Zhizhesihai (Beijing) Technology Limited.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 use core::codec::codec_util::index_header_length;
 use core::codec::codec_util::*;
 use core::codec::compressing::matching_reader::MatchingReaders;
